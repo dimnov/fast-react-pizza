9106f01a776d231014ff99cabf48d9b143a9064a
dimnov <dimnov1512@gmail.com>
2024-05-30 22:38:35 +0300

    fetching the menu and then showing each item

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,7 +1,17 @@
+import { useLoaderData } from "react-router-dom";
 import { getMenu } from "../../services/apiRestaurant";
+import MenuItem from "./MenuItem";
 
 function Menu() {
-  return <h1>Menu</h1>;
+  const menu = useLoaderData();
+
+  return (
+    <ul>
+      {menu.map((pizza) => (
+        <MenuItem key={pizza.id} pizza={pizza} />
+      ))}
+    </ul>
+  );
 }
 
 export async function loader() {
