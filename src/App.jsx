1e71b4caed6136ebb9d366959920f3ca82ceb2de
dimnov <dimnov1512@gmail.com>
2024-06-01 23:22:46 +0300

    add error handler

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,16 @@ import Home from "./ui/Home";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import Order, { loader as orderLoader } from "./features/order/Order";
-import CreateOrder, { action as createOrderAction } from "./features/order/CreateOrder";
+import CreateOrder, {
+  action as createOrderAction,
+} from "./features/order/CreateOrder";
 import AppLayout from "./ui/AppLayout";
 import Error from "./ui/Error";
 
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
+    errorElement: <Error />,
 
     children: [
       {
