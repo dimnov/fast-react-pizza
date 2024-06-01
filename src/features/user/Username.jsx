151706737b83e0deabc7682228b73cbc34919726
dimnov <dimnov1512@gmail.com>
2024-06-01 22:00:35 +0300

    added flexbox

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="hidden text-sm font-semibold">Testtest</div>;
+  return <div className="hidden text-sm font-semibold md:block">Testtest</div>;
 }
 
 export default Username;
