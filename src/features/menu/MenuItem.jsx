b4d208de602d799b23cf3388426b6f77019e1d65
dimnov <dimnov1512@gmail.com>
2024-06-02 18:53:09 +0300

    Add different styles for the custom button

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,3 +1,4 @@
+import Button from "../../ui/Button";
 import { formatCurrency } from "../../utils/helpers";
 
 function MenuItem({ pizza }) {
@@ -11,18 +12,24 @@ function MenuItem({ pizza }) {
         className={`h-24 ${soldOut ? "opacity-60 grayscale" : ""}`}
       />
 
-      <div className="flex flex-col">
+      <div className="mt-1 flex grow flex-col">
         <p className="font-medium">{name}</p>
         <p className="text-sm capitalize italic text-stone-500">
           {ingredients.join(", ")}
         </p>
-        <div className="mt-auto">
+        <div className="mt-auto flex items-center justify-between">
           {!soldOut ? (
             <p className="text-sm">{formatCurrency(unitPrice)}</p>
           ) : (
             <p className="text-sm font-medium uppercase text-stone-500">
               Sold out
             </p>
+          )}
+
+          {!soldOut && (
+            <Button type="small" to={""}>
+              Add to cart
+            </Button>
           )}
         </div>
       </div>
