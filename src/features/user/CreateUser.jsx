7597913973e72a8e0548c5241c9dcc9a99cddfbe
dimnov <dimnov1512@gmail.com>
2024-06-03 18:09:44 +0300

    setting the name and showing errors

--- src/features/user/CreateUser.jsx
+++ src/features/user/CreateUser.jsx
@@ -1,11 +1,29 @@
 import { useState } from "react";
 import Button from "../../ui/Button";
+import { useDispatch } from "react-redux";
+import { updateName } from "./userSlice";
+import { useNavigate } from "react-router-dom";
 
 function CreateUser() {
   const [username, setUsername] = useState("");
+  const [error, setError] = useState("");
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   function handleSubmit(e) {
     e.preventDefault();
+
+    if (!username) {
+      return setError("Please enter your name.");
+    }
+
+    if (username.length < 3) {
+      return setError("The username should be at least 3 characters long.");
+    }
+
+    dispatch(updateName(username));
+    navigate("/menu");
   }
 
   return (
@@ -13,6 +31,8 @@ function CreateUser() {
       <p className="mb-4 text-sm text-stone-600 md:text-base">
         👋 Welcome! Please start by telling us your name:
       </p>
+
+      <p className="mb-4 text-sm text-red-600 md:text-base">{error}</p>
 
       <input
         type="text"
