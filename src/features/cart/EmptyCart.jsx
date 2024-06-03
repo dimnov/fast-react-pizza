3e2aed0282204093f4c7d750467b08d203ad73ee
dimnov <dimnov1512@gmail.com>
2024-06-04 00:47:12 +0300

    Change the style

--- src/features/cart/EmptyCart.jsx
+++ src/features/cart/EmptyCart.jsx
@@ -1,11 +1,15 @@
-import { Link } from 'react-router-dom';
+import Button from "../../ui/Button";
 
 function EmptyCart() {
   return (
-    <div>
-      <Link to="/menu">&larr; Back to menu</Link>
+    <div className="flex flex-col items-center justify-center">
+      <p className="mb-8 mt-12 font-semibold">
+        Your cart is still empty. Start adding some pizzas :)
+      </p>
 
-      <p>Your cart is still empty. Start adding some pizzas :)</p>
+      <Button type={"primary"} to="/menu">
+        &larr; Back to menu
+      </Button>
     </div>
   );
 }
