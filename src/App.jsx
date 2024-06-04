a149e4322782cd140efc974919a674da87dc3d4c
dimnov <dimnov1512@gmail.com>
2024-06-04 20:30:21 +0300

    Add a way to give priority after order creation

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Home from "./ui/Home";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import Order, { loader as orderLoader } from "./features/order/Order";
+import { action as updateOrderAction } from "./features/order/UpdateOrder";
+
 import CreateOrder, {
   action as createOrderAction,
 } from "./features/order/CreateOrder";
@@ -39,6 +41,7 @@ const router = createBrowserRouter([
         element: <Order />,
         loader: orderLoader,
         errorElement: <Error />,
+        action: updateOrderAction,
       },
     ],
   },
